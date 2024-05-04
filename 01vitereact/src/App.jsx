d258464e058de8fd77be4ca4c1d0db57a003e73c
Birbal Kumar <birbalkr1435@gmail.com>
2024-05-04 22:29:41 +0530

    useCallback, useEffect, useRef, useState

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,41 +1,60 @@
 
-import { useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
 
 function App() {
+const [length, setLength]=useState(8)
+const [number, setNumber]=useState(false)
+const [charAllow, setCharAllow]=useState(false)
+const [password, setPassword]=useState("")
 
-  const [color, setColor]=useState("olive")
+const passwordref=useRef(null)
 
-  // let [value, setValue] = useState(15)
+const PasswordGenerator = useCallback(() => {
 
-  // // let value=5
-  // const AddValue=()=>{
-  //   value = value+1
-  //   setValue(value)
-  // }
-  // const removeVale=()=>{
-  //   value=value-1
-  //   setValue(value)
-  // }
+  let pass=""
+  let str = "qwertyuioplkjhgfdsazxcvbnmZXCVBNMLPOKIJUHYGTFRDESWAQ"
+  if(number) str+="0123456789"
+  if(charAllow) str+= "!@#$%^&*"
+
+  for (let i = 0; i < length; i++) {
+    let char = Math.floor(Math.random()*str.length+1)
+    pass += str.charAt(char)
+  }
+
+  setPassword(pass)
+},
+[length,number,charAllow,setPassword])
+
+const CopyPassword =useCallback(()=>{
+  window.navigator.clipboard.writeText(password)
+  passwordref.current?.select();
+},[password])
+
+useEffect(() =>{
+  PasswordGenerator()
+},[length,number,charAllow,PasswordGenerator])
 
   return (
-    
-    <div className="w-full h-screen duration-200"
-      style={{backgroundColor: color}}>
-        <div className='fixed flex flex-warp justify-center bottom-12 inset-x-0'>
-        <div className='fixed flex flex-warp justify-center gap-3 shadow-lg px-3 py-2 rounded-3xl bg-white'>
-            <button onClick={()=>setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{background:"red"}}>Red</button>
-            <button onClick={()=>setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{background:"green"}}>Green</button>
-            <button onClick={()=>setColor("black")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{background:"black"}}>Black</button>
-            <button onClick={()=>setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{background:"yellow"}}>Yellow</button>
-            <button onClick={()=>setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{background:"Blue"}}>Blue</button>
-
-        </div>
-        </div>
+   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-3 bg-gray-800 text-orange-500'>
+    <h1 className='text-4xl text-center '>Password Generator</h1>
+    <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+      <input type='text' value={password}  className='outline-none w-full py-1 px-3' placeholder='Password' readOnly ref={passwordref} />
+      <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={CopyPassword}>Copy</button>
+    </div>
+    <div className='flex text-sm gap-2'>
+      <div className='flex items-center gap-x-1'>
+        <input type="range" min={6} max={20} value={length} className='cursor-pointer' onChange={(e)=>{setLength(e.target.value)}}/><label>Lenght: {length}</label>
       </div>
-
-    
+      <div className='flex items-center gap-x-1'>
+      <input type="checkbox" defaultChecked={number}id='numberInput'  onChange={()=>{setNumber((prev)=>!prev)}}/><label>Number</label>
+      </div>
+      <div className='flex items-center gap-x-1'>
+      <input type="checkbox" defaultChecked={number}id='numberInput'  onChange={()=>{setCharAllow((prev)=>!prev)}}/><label>Characters</label>
+      </div>
+    </div>
+   </div>
   )
 }
 
